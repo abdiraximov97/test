1fc6fd8a28135c7c5351e3e591a3bba21fb3716e
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2022-01-17 16:14:49 +0500

    add BanValidation

--- src/Validations/AdminValidation.js
+++ src/Validations/AdminValidation.js
@@ -1,17 +1,1 @@
-const Joi = require("joi");
 
-module.exports = class AdminValidations {
-  static async AdminLoginValidation(data, CustomError) {
-    return await Joi.object({
-      user_login: Joi.string()
-        .min(4)
-        .max(64)
-        .required()
-        .error(new CustomError(400, "Login xato!")),
-      user_password: Joi.string()
-        .required()
-        .min(4)
-        .error(new CustomError(400, "Parol xato!")),
-    }).validateAsync(data);
-  }
-};
---
+++ src/Validations/BanValidation.js
@@ -0,0 +1,22 @@
+
+const Joi = require("joi");
+
+module.exports = class BanValidations {
+	static async CreateBanValidation(data, CustomError) {
+		return await Joi.object({
+			user_id: Joi.string()
+				.required()
+				.uuid()
+				.error(new CustomError(400, "User id is invalid")),
+			ban_reason: Joi.string()
+				.required()
+				.min(10)
+				.max(1024)
+				.error(new CustomError(400, "Reason is invalid")),
+			ban_expire_date: Joi.date()
+				.required()
+				.min(new Date())
+				.error(new CustomError(400, "Expire date is invalid")),
+		}).validateAsync(data);
+	}
+};
