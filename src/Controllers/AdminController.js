97b561825fa75c31f9ea8b2668a5aeaceb83b643
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2022-01-17 16:35:51 +0500

    add information adminController

--- src/Controllers/AdminController.js
+++ src/Controllers/AdminController.js
@@ -1,7 +1,37 @@
-const { UserLoginValidation } = require("../Validations/UserValidation");
+const { CreateBanValidation } = require("../Validations/BanValidation");
 
 module.exports = class AdminController {
-    static async CreateBanController(req, res, next) {
-        const data = await UserLoginValidation(req.body, res.error);
+    static async CreateBanController(req, res, next) {  
+        try {
+            const data = await CreateBanValidation(req.body, res.error);
+        
+        const user = await req.db.users.findOne({
+            where: {
+                user_id: data.user_id,
+            }
+        });
+
+        if(!user) throw new res.error(404, "User topilmadi!");
+
+        if(user.role == "admin") throw new res.error(404, "Siz adminga ta'qiq qo'yolmaysiz");
+
+        const ban = await req.db.user_bans.create({
+            user_id: data.user_id,
+            ban_reason: data.ban_reason,
+            ban_expire_date: data.ban_expire_date,
+        });
+
+        res.status(201).json({
+            ok: true,
+            message: "Ta'qiq qo'yildi",
+            data: {
+                ban,
+            },
+        });
+        } catch (error) {
+            console.error("AdminController error: ", error);
+            next(error);
+        }
+
     };
 };
--- src/Validations/BanValidation.js
+++ src/Validations/BanValidation.js
@@ -7,16 +7,16 @@ module.exports = class BanValidations {
 			user_id: Joi.string()
 				.required()
 				.uuid()
-				.error(new CustomError(400, "User id is invalid")),
+				.error(new CustomError(400, "User id noto'g'ri!")),
 			ban_reason: Joi.string()
 				.required()
 				.min(10)
 				.max(1024)
-				.error(new CustomError(400, "Reason is invalid")),
+				.error(new CustomError(400, "Ta'qiq berishingiz sababi noto'g'ri!")),
 			ban_expire_date: Joi.date()
 				.required()
 				.min(new Date())
-				.error(new CustomError(400, "Expire date is invalid")),
+				.error(new CustomError(400, "Ta'qiqning tugash vaqti noto'g'ri!")),
 		}).validateAsync(data);
 	}
 };
