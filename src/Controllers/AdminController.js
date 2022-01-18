c18ea62f7aae534e93a231669ab433126e7f79c1
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2022-01-18 09:30:36 +0500

    fix adminController

--- src/Controllers/AdminController.js
+++ src/Controllers/AdminController.js
@@ -1,37 +1,58 @@
 const { CreateBanValidation } = require("../Validations/BanValidation");
 
 module.exports = class AdminController {
-    static async CreateBanController(req, res, next) {  
-        try {
-            const data = await CreateBanValidation(req.body, res.error);
-        
-        const user = await req.db.users.findOne({
-            where: {
-                user_id: data.user_id,
-            }
-        });
+  static async CreateBanController(req, res, next) {
+    try {
+      const data = await CreateBanValidation(req.body, res.error);
 
-        if(!user) throw new res.error(404, "User topilmadi!");
+      const user = await req.db.users.findOne({
+        where: {
+          user_id: data.user_id,
+        },
+      });
 
-        if(user.role == "admin") throw new res.error(404, "Siz adminga ta'qiq qo'yolmaysiz");
+      if (!user) throw new res.error(404, "User topilmadi!");
 
-        const ban = await req.db.user_bans.create({
-            user_id: data.user_id,
-            ban_reason: data.ban_reason,
-            ban_expire_date: data.ban_expire_date,
-        });
+      if (user.role == "admin")
+        throw new res.error(404, "Siz adminga ta'qiq qo'yolmaysiz");
 
-        res.status(201).json({
-            ok: true,
-            message: "Ta'qiq qo'yildi",
-            data: {
-                ban,
-            },
-        });
-        } catch (error) {
-            console.error("AdminController error: ", error);
-            next(error);
-        }
+      const ban = await req.db.user_bans.create({
+        user_id: data.user_id,
+        ban_reason: data.ban_reason,
+        ban_expire_date: data.ban_expire_date,
+      });
 
+      res.status(201).json({
+        ok: true,
+        message: "Ta'qiq qo'yildi",
+        data: {
+          ban,
+        },
+      });
+    } catch (error) {
+      console.error("AdminController error: ", error);
+      next(error);
+    }
+  }
+
+  static async DeleteBanController(req, res, next) {
+    try {
+      const ban_id = req.params.ban_id;
+
+      const ban = await req.db.users_bans.destroy({
+        where: {
+          ban_id: ban_id,
+        },
+      });
+
+      res.status(200).json({
+        ok: true,
+        message: "Ta'qiq olib tashlandi",
+      });
+    } catch (error) {
+      console.error("DeleteBanController error: ", error);
     };
+  };
+
+  
 };
